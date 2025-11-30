752e204e669e71bf13d3522d1ae93d627749f24a
MT-Patriot1776 <cheshierm@gmail.com>
2025-11-29 22:25:18 -0700

    Fixed SPT Version and metadata

--- SOCOM_sourceCode/src/AddTraderWithAssortJson.cs
+++ SOCOM_sourceCode/src/AddTraderWithAssortJson.cs
@@ -21,7 +21,7 @@ public record ModMetadata : AbstractModMetadata
     public override List<string>? Contributors { get; init; } = ["Srispt"];
     public override SemanticVersioning.Version Version { get; init; } = new("2.0.0");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
-    public override List<string>? Incompatibilities { get; init; } = ["null"];
+    public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
     public override string? Url { get; init; } = "https://github.com/MT-Patriot1776/SPT-SOCOM";
     public override bool? IsBundleMod { get; init; } = false;
